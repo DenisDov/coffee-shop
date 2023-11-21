5cfd36da133a4b8d5dc1ea93eceeb8638441b99a
DenisDov <dovzdenis@gmail.com>
2023-11-21 15:55:17 +0200

    PlatformPressable button

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,17 @@
+import { getDefaultHeaderHeight } from '@react-navigation/elements';
+import {
+  useSafeAreaFrame,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
 import { Box, Text } from '@/theme';
 
 export default function HomeScreen() {
+  const frame = useSafeAreaFrame();
+  const insets = useSafeAreaInsets();
+
+  const headerHeight = getDefaultHeaderHeight(frame, false, insets.top);
+  console.log('headerHeight: ', headerHeight);
   return (
     <Box flex={1} backgroundColor="background">
       <Text>HomeScreen</Text>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
+import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
 import React from 'react';
-import { Pressable, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { Text, theme } from '@/theme';
 
@@ -9,29 +10,19 @@ type Props = {
   title?: string;
 };
 
-const RIPPLE_CONFIG = {
-  color: theme.colors.primary,
-  borderless: true,
-};
-
 export const Button = ({ onPress, title }: Props) => {
   const handlePress = () => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
     onPress();
   };
   return (
-    <Pressable
+    <PlatformPressable
       onPress={handlePress}
       hitSlop={16}
-      android_ripple={RIPPLE_CONFIG}
-      style={({ pressed }) => [
-        styles.button,
-        {
-          opacity: pressed ? 0.7 : 1,
-        },
-      ]}>
+      style={styles.button}
+      pressOpacity={0.7}>
       <Text variant="button">{title}</Text>
-    </Pressable>
+    </PlatformPressable>
   );
 };
 
