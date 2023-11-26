a64185e9604a5544d196dbf8a5d0e0fdb3d7d056
DenisDov <dovzdenis@gmail.com>
2023-11-26 13:35:25 +0200

    section list trick

--- src/components/SectionList.tsx
+++ src/components/SectionList.tsx
@@ -3,35 +3,28 @@ import { SafeAreaView, SectionList, StatusBar, StyleSheet } from 'react-native';
 
 import { Box, Text } from '@/theme';
 
-const DATA = [
-  {
-    title: 'Main dishes',
-    data: ['Pizza', 'Burger', 'Risotto'],
-  },
-  {
-    title: 'Sides',
-    data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-  },
-  {
-    title: 'Drinks',
-    data: ['Water', 'Coke', 'Beer'],
-  },
-  {
-    title: 'Desserts',
-    data: ['Cheese Cake', 'Ice Cream'],
-  },
-];
+const items = Array.from({ length: 50 }, (_, index) => `Item ${index + 1}`);
 
 const MySectionList = () => (
   <SectionList
-    sections={DATA}
-    keyExtractor={(item, index) => item + index}
+    sections={[{ data: items }]}
+    ListHeaderComponent={() => (
+      <Box height={300} backgroundColor="error">
+        <Text>Banner</Text>
+      </Box>
+    )}
+    keyExtractor={item => item}
     renderItem={({ item }) => (
       <Box>
         <Text>{item}</Text>
       </Box>
     )}
-    renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
+    renderSectionHeader={() => (
+      <Box style={{ height: 40, backgroundColor: 'lime' }}>
+        <Text>HORIZONTAL_FLATLIST</Text>
+      </Box>
+    )}
+    stickySectionHeadersEnabled
   />
 );
 
