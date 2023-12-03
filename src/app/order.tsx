48f690be5f4044b1d56ebabf96ce3fef49d22ade
DenisDov <dovzdenis@gmail.com>
2023-12-03 21:01:48 +0200

    add stepper

--- src/app/order.tsx
+++ src/app/order.tsx
@@ -7,6 +7,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Button } from '@/components/Button';
 import { Header } from '@/components/Header';
 import { OrderControl } from '@/components/OrderControl';
+import Stepper from '@/components/Stepper';
 import { Box, Text } from '@/theme';
 
 export default function OrderScreen() {
@@ -109,12 +110,13 @@ export default function OrderScreen() {
                   </Box>
                 </Box>
               </Box>
-              <Box width={100} backgroundColor="debug" alignItems="flex-end">
-                <Box flexDirection="row" alignItems="center" gap="sm">
+              <Box width={100} alignItems="flex-end">
+                <Stepper />
+                {/* <Box flexDirection="row" alignItems="center" gap="sm">
                   <Text>-</Text>
                   <Text>1</Text>
                   <Text>+</Text>
-                </Box>
+                </Box> */}
               </Box>
             </Box>
           </Box>
---
+++ src/components/Stepper.tsx
@@ -0,0 +1,78 @@
+import { PlatformPressable } from '@react-navigation/elements';
+import { Image } from 'expo-image';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Box, Text } from '@/theme';
+
+export const Stepper = () => {
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <Box flexDirection="row" alignItems="center">
+      <PlatformPressable
+        onPress={handleDecrement}
+        hitSlop={16}
+        style={styles.button}>
+        <Image
+          style={[
+            styles.image,
+            { tintColor: count < 2 ? '#AAADB0' : '#2F2D2C' },
+          ]}
+          source={require('@/assets/icons/svg/minus.svg')}
+          contentFit="contain"
+        />
+      </PlatformPressable>
+
+      <Box width={30}>
+        <Text
+          fontVariant={['tabular-nums']}
+          textAlign="center"
+          variant="semiBold"
+          fontSize={14}>
+          {count}
+        </Text>
+      </Box>
+
+      <PlatformPressable
+        onPress={handleIncrement}
+        hitSlop={16}
+        style={styles.button}>
+        <Image
+          style={styles.image}
+          source={require('@/assets/icons/svg/plus.svg')}
+          contentFit="contain"
+          tintColor="#2F2D2C"
+        />
+      </PlatformPressable>
+    </Box>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 28,
+    height: 28,
+    borderRadius: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#EAEAEA',
+  },
+  image: {
+    width: 16,
+    height: 16,
+  },
+});
+
+export default Stepper;
