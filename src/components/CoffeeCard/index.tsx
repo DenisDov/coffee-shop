8559572351789159e21cd6627d02a379b950c829
DenisDov <dovzdenis@gmail.com>
2023-12-09 13:44:41 +0200

    CoffeeCard android fix

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -32,75 +32,72 @@ export const CoffeeCard = memo(
     };
 
     return (
-      <PlatformPressable
-        onPress={navTo}
-        style={[styles.pressable, { width: cardWidth }]}>
-        <Box padding="xs">
-          <ImageBackground
-            style={styles.imageBackground}
-            source={image.source}
-            placeholder={{ thumbhash: image.thumbhash }}
-            contentFit="cover"
-            transition={1000}>
-            <Box flexDirection="row" alignItems="center" gap="xxs">
-              <Image
-                style={{ width: 10, height: 10 }}
-                source={require('@/assets/icons/svg/star.svg')}
-                contentFit="contain"
-              />
-              <Text variant="semiBold" fontSize={10} color="white">
-                {rating}
+      <Box
+        style={{
+          borderRadius: 16,
+          overflow: 'hidden',
+          backgroundColor: 'white',
+          width: cardWidth,
+        }}>
+        <PlatformPressable onPress={navTo}>
+          <Box padding="xs">
+            <ImageBackground
+              style={styles.imageBackground}
+              source={image.source}
+              placeholder={{ thumbhash: image.thumbhash }}
+              contentFit="cover"
+              transition={1000}>
+              <Box flexDirection="row" alignItems="center" gap="xxs">
+                <Image
+                  style={{ width: 10, height: 10 }}
+                  source={require('@/assets/icons/svg/star.svg')}
+                  contentFit="contain"
+                />
+                <Text variant="semiBold" fontSize={10} color="white">
+                  {rating}
+                </Text>
+              </Box>
+            </ImageBackground>
+          </Box>
+
+          <Box padding="sm" gap="s">
+            <Box>
+              <Text numberOfLines={1} variant="semiBold">
+                {title}
+              </Text>
+              <Text numberOfLines={1} fontSize={12} color="muted">
+                with {extras}
               </Text>
             </Box>
-          </ImageBackground>
-        </Box>
-
-        <Box padding="sm" gap="s">
-          <Box>
-            <Text numberOfLines={1} variant="semiBold">
-              {title}
-            </Text>
-            <Text numberOfLines={1} fontSize={12} color="muted">
-              with {extras}
-            </Text>
+            <Box
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="space-between">
+              <Text numberOfLines={1} variant="semiBold" fontSize={18}>
+                $ {price}
+              </Text>
+              <PlatformPressable
+                onPress={onAddToCart}
+                hitSlop={16}
+                style={styles.button}>
+                <Image
+                  style={{
+                    width: 16,
+                    height: 16,
+                  }}
+                  source={require('@/assets/icons/svg/plus.svg')}
+                  contentFit="contain"
+                />
+              </PlatformPressable>
+            </Box>
           </Box>
-          <Box
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="space-between">
-            <Text numberOfLines={1} variant="semiBold" fontSize={18}>
-              $ {price}
-            </Text>
-            <PlatformPressable
-              onPress={onAddToCart}
-              hitSlop={16}
-              style={styles.button}>
-              <Image
-                style={{
-                  width: 16,
-                  height: 16,
-                }}
-                source={require('@/assets/icons/svg/plus.svg')}
-                contentFit="contain"
-              />
-            </PlatformPressable>
-          </Box>
-        </Box>
-      </PlatformPressable>
+        </PlatformPressable>
+      </Box>
     );
   },
 );
 
 const styles = StyleSheet.create({
-  pressable: {
-    backgroundColor: 'white',
-    borderRadius: 16,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    elevation: 3,
-  },
   imageBackground: {
     padding: 8,
     borderRadius: 16,
