2e4343bf77b3801b6352befd34ffab12cad236c1
DenisDov <dovzdenis@gmail.com>
2023-12-12 14:20:11 +0200

    rm styles from SegmentedControl

--- src/components/OrderControl.tsx
+++ src/components/OrderControl.tsx
@@ -21,14 +21,6 @@ export const OrderControl = () => {
       onChange={_onChange}
       onValueChange={_onValueChange}
       selectedIndex={selectedIndex}
-      tintColor="#C67C4E"
-      backgroundColor="#F2F2F2"
-      fontStyle={{ fontSize: 16 }}
-      activeFontStyle={{
-        fontSize: 16,
-        color: '#FFFFFF',
-      }}
-      style={{ height: 40 }}
     />
   );
 };
